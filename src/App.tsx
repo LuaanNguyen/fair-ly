642a98078a14d5d731540cd3dcf60be686603497
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-10-07 11:03:39 -0700

    Add: new header 🔝

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Settings, X, Flag } from "lucide-react";
+
 function App() {
   return (
-    <>
-      <h1 className="">Hello</h1>
-    </>
+    <main className="w-[400px] h-[400px] bg-black border-2 border-white text-white ">
+      <section className="p-4 flex flex-col">
+        <nav className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img src="/whitelogo.svg" className="w-[50px] aspect-square"></img>
+            <h1 className="text-2xl font-semibold">Bias Analyzer</h1>
+          </div>
+          <div className="flex gap-3">
+            <Flag className="cursor-not-allowed" />
+            <Settings className="cursor-not-allowed " />
+            <X className="cursor-pointer" onClick={() => window.close()} />
+          </div>
+        </nav>
+      </section>
+    </main>
   );
 }
 
