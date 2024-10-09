87ed7f7401bacd2c7969d56ca1ba2b55175ab643
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-10-09 10:20:11 -0700

    Added Loader Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Settings, X, Flag } from "lucide-react";
+import Loader from "./components/Loader";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <X className="cursor-pointer" onClick={() => window.close()} />
           </div>
         </nav>
+        <Loader />
       </section>
     </main>
   );
---
+++ src/components/Loader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader py-10">
+      <div className="box box-1">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-2">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-3">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-4">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+    </div>
+  );
+}
